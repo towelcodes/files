2984ee0794d6b420126a82468ddb94f457be44c7
teatowel <tea@towel.codes>
2025-11-22 15:18:01 +0000

    add temporary favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,19 @@
 <script lang="ts">
     import { PUBLIC_REPO_URL } from "$env/static/public";
     import "../app.css";
-    import favicon from "$lib/assets/favicon.svg";
+    import faviconDark from "$lib/assets/favicon-dark.svg";
+    import faviconLight from "$lib/assets/favicon-light.svg";
 
     let { children } = $props();
 </script>
 
 <svelte:head>
-    <link rel="icon" href={favicon} />
+    <link rel="icon" href={faviconDark} media="(prefers-color-scheme: dark)" />
+    <link
+        rel="icon"
+        href={faviconLight}
+        media="(prefers-color-scheme: light)"
+    />
 </svelte:head>
 
 {@render children?.()}
